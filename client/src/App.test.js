ea8a2de6d4eff644b5563ff227e9567a0be26be8
Krimsit <krimsitdjenkis@gmail.com>
2023-01-30 18:48:38 +0300

    [master] Add project files

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -16,15 +16,11 @@ test("login test", async () => {
   const usernameInput = screen.getByPlaceholderText("Введите имя")
   const loginButton = screen.getByText("Войти")
 
-  screen.debug()
-
   expect(usernameInput).toBeInTheDocument();
 
   fireEvent.change(usernameInput, { target: { value: "Test user" } })
 
   fireEvent.click(loginButton)
-
-  screen.debug()
 
   expect(screen.getByText(/Чат/i)).toBeInTheDocument()
 })
